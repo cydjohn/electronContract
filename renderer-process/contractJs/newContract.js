c0c7acb965be3108734d963c514401c116263240
Yudong Cao <cydjohn@live.com>
2018-07-25 17:33:04 -0400

    update new contract

--- renderer-process/contractJs/newContract.js
+++ renderer-process/contractJs/newContract.js
@@ -6,40 +6,47 @@ const { BrowserWindow } = require('electron').remote
 const addNewContractButton = document.getElementById("add-new")
 
 const addNewStageButton = document.getElementById("add-stages")
-
+var counter = 0
+var stages = []
 
 addNewStageButton.addEventListener('click', () => {
-    console.log("adsfasdfas");
     addNewStage();
-
+    getStages();
 })
 
 
 function addNewStage() {
     // document.getElementById('add-stages').innerHTML = "";
+    counter += 1;
     document.getElementById('stages').innerHTML +=
         '<div class="row col-lg-12 col-md-12 m-1">' +
         '<div class="input-group col-lg-2 col-md-2">' +
         '<div class="input-group-prepend">' +
-        '<span class="input-group-text"> 期数：</span>'+
-            '</div >'+
-        '<input type="text" class="form-control" aria-label="Default" aria-describedby="inputGroup-sizing-default" id="stages-input">' +
+        '<span class="input-group-text"> 期数：</span>' +
+        '</div >' +
+        '<input type="text" class="form-control" aria-label="Default" aria-describedby="inputGroup-sizing-default" value = '+ counter +' disabled="disabled" id="stages-input-' + counter + '">' +
         '</div>' +
 
         '<div class="input-group col-lg-5 col-md-5">' +
-            '<div class="input-group-prepend">'+
-                '<span class="input-group-text">付款时间：</span>'+
-            '</div>' +
-            '<input type="date" class="form-control" aria-label="Default" aria-describedby="inputGroup-sizing-default" id="time-input">' +
+        '<div class="input-group-prepend">' +
+        '<span class="input-group-text">付款时间：</span>' +
+        '</div>' +
+        '<input type="date" class="form-control" aria-label="Default" aria-describedby="inputGroup-sizing-default" id="time-input-' + counter + '">' +
         '</div>' +
 
 
-            '<div class="input-group col-lg-4 col-md-4">' +
-                '<div class="input-group-prepend">' +
-                    '<span class="input-group-text">付款金额：</span>' +
-                '</div>' +
-                '<input type="text" class="form-control" aria-label="Default" aria-describedby="inputGroup-sizing-default" id="amount-input">'+
+        '<div class="input-group col-lg-4 col-md-4">' +
+        '<div class="input-group-prepend">' +
+        '<span class="input-group-text">付款金额：</span>' +
         '</div>' +
-            '</div> '
-        }
+        '<input type="float" class="form-control" aria-label="Default" aria-describedby="inputGroup-sizing-default" id="amount-input-' + counter + '">' +
+        '</div>' +
+        '</div> '
+}
+
+function getStages() {
+    var i = 0;
+    for (i=0;i<=counter;i++) {
         
+    }
+}
