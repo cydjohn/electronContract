c8714425a1f679e2376c06257d41978649533130
Yudong Cao <cydjohn@live.com>
2018-07-26 20:18:52 -0400

    update

---
+++ renderer-process/contractJs/finishedTable.js
@@ -0,0 +1,41 @@
+const { BrowserWindow, dialog } = require('electron').remote
+const path = require('path')
+const { ipcRenderer } = require('electron')
+var moment = require('moment');
+// const newWindowBtn = document.getElementById('new')
+
+const XLSX = require('xlsx')
+
+let allData = []
+
+let tableData = []
+// loadData()
+
+ipcRenderer.send('request-all-data')
+
+ipcRenderer.on('get-all-data', (event, arg) => {
+    allData = arg;
+
+    //temp
+    tableData = allData;
+    // console.log(tableData);
+    loadData();
+})
+
+
+
+function loadData() {
+    document.getElementById('finished-table-data').innerHTML = ""
+    var d = 0
+    for (d in tableData) {
+        
+        document.getElementById('finished-table-data').innerHTML +=
+            "<tr>" +
+            "<td>" + (parseInt(d) + 1) + "</td>" +
+            "<td>" + tableData[d].secondParty + "</td>" +
+            "<td>" + tableData[d].stageSum + "</td>" +
+            "<td>" + tableData[d].stages.pop().time + "</td>" +
+            "</tr>"
+    }
+}
+
