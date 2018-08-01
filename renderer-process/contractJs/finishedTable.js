7fd618d8d28b9081cb23dfb58cbda36288358a2a
Yudong Cao <cydjohn@live.com>
2018-08-01 18:59:02 -0400

    fix finished table

--- renderer-process/contractJs/finishedTable.js
+++ renderer-process/contractJs/finishedTable.js
@@ -47,17 +47,29 @@ function loadData() {
   document.getElementById('finished-table-data').innerHTML = ""
   var d = 0
   for (d in finishedTableData) {
-    let s = finishedTableData[d].stages;
-    s.sort();
-    let lastDay = s[s.length - 1];
-    if (moment(new Date()).isBefore(moment(lastDay))) {
+    var s = 0;
+    var isFinished = false;
+    var lastDay = finishedTableData[d].stages[0].time;
+    for(s in finishedTableData[d].stages) {
+      if (moment(new Date()).isBefore(moment(finishedTableData[d].stages[s].time))) {
+        isFinished = false;
+      }
+      else {
+        isFinished = true;
+      }
+      if (moment(lastDay).isBefore(moment(finishedTableData[d].stages[s].time))) {
+        lastDay = finishedTableData[d].stages[s].time;
+      }
+    }
+    
+    if (isFinished) {
       document.getElementById('finished-table-data').innerHTML +=
         "<tr>" +
         "<td>" + (parseInt(d) + 1) + "</td>" +
         "<td>" + finishedTableData[d].contractNumber + "</td>" +
         "<td>" + finishedTableData[d].secondParty + "</td>" +
         "<td>" + finishedTableData[d].stageSum + "</td>" +
-        "<td>" + lastDay.time + "</td>" +
+        "<td>" + lastDay + "</td>" +
         "</tr>"
     }
   }
