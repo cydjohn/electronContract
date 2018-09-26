aacfdedbb93c1b8ffd06944a8ae02c05fd5ca76d
cydjohn <ycao@goodlooking.io>
2018-09-26 14:23:25 -0500

    angular init

--- windowsInstaller.js
+++ windowsInstaller.js
@@ -1,15 +1,15 @@
 var electronInstaller = require('electron-winstaller');
 const path = require('path')
 resultPromise = electronInstaller.createWindowsInstaller({
-    appDirectory: './out/合同管理系统-win32-ia32',
+    appDirectory: './out/借款系统-win32-ia32',
     outputDirectory: './windows32',
     authors: 'cyd',
-    exe: '合同管理系统.exe',
+    exe: '借款系统.exe',
     noMsi: true,
     setupExe: 'ElectronLoanSetup.exe',
     setupIcon: path.join('assets', 'app-icon', 'win', 'app.ico'),
     skipUpdateIcon: true,
-    name: "ElectronContract"
+    name: "ElectronLoan"
   });
 
 resultPromise.then(() => console.log("It worked!"), (e) => console.log(`No dice: ${e.message}`));
