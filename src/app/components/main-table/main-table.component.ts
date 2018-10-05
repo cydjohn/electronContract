e0c0c737f38093334d99c17714e3a8ad765e9f3a
cydjohn <cydjohn@live.com>
2018-10-05 13:49:02 -0500

    ~~~

--- src/app/components/main-table/main-table.component.ts
+++ src/app/components/main-table/main-table.component.ts
@@ -11,22 +11,45 @@ export class MainTableComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    this.calculateSum();
   }
 
   showDetail(rowData) {
     console.log(rowData);
+    
   }
 
-  tableData: Contract[] = [{
-    contractNumber: "fasfasdf",
-    firstParty: "adsf",
-    secondParty: "fasdf",
-    startTime: "fdsa",
-    carType: "fasd",
-    quantity: 10,
-    stageSum: 10,
-    amountSum: 10,
-    stages: []
-  }]
+  calculateSum() {
+    for (let d in this.tableData) {
+      this.amountSum += this.tableData[d].stageSum;
+    }
+  }
+
+  amountSum = 0
+
+  tableData: Contract[] = [
+    {
+      contractNumber: "fasfasdf",
+      firstParty: "adsf",
+      secondParty: "fasdf",
+      startTime: "fdsa",
+      carType: "fasd",
+      quantity: 10,
+      stageSum: 10,
+      amountSum: 10,
+      stages: []
+    },
+    {
+      contractNumber: "fasfasdf",
+      firstParty: "adsf",
+      secondParty: "fasdf",
+      startTime: "fdsa",
+      carType: "fasd",
+      quantity: 10,
+      stageSum: 10,
+      amountSum: 10,
+      stages: []
+    }
+  ]
 
 }
